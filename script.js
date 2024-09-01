47eda15df8b2c6e19c2182a28fd7e19a89e8d65f
atharvawadekar123 <theteckky@gmail.com>
2024-09-01 22:23:15 +0530

    MovieHunt 3.3

--- script.js
+++ script.js
@@ -1,42 +1,103 @@
 const currentYear = new Date().getFullYear();
 document.getElementById("currentYear").innerText = currentYear + " Movies";
 
-const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&primary_release_year=${currentYear}`;
+const urlYear = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&primary_release_year=${currentYear}`;
 
 var cardsOfTheYear = [];
+var cardsOfTheGenres = {};
 	
-	async function load2024Cards()
-	{
-		let response = await fetch(url);
+	
+	async function loadYearCards()
+	{	
+		let response = await fetch(urlYear);
 		let data = await response.json();
-		await loadMovieGenres();
 		
 		for(index in data.results)
 		{	
 			insertCard(cardsOfTheYear,index,data,"2024");
-		}	
+		}
 	}
 	
-	load2024Cards();
+	async function exec()
+	{
+		await loadMovieGenres();
+		loadYearCards();
 
-		const sliderContainer = document.querySelector('.movies-section .movies-slider');
-		const prevBtn = document.getElementById('prev-btn');
-		const nextBtn = document.getElementById('next-btn');
-		
-		prevBtn.onclick = function(){
-			console.log('Previous button clicked!');
-			sliderContainer.scrollBy({
-				left: -460,
-				behavior: 'smooth'
-			});
+		if(localStorage.getItem('genres')!==null && localStorage.getItem('genres')!==[] )
+		{
+			let genreArray = JSON.parse(localStorage.getItem('genres'));
+			console.log(genreArray);
+
+			for(genre of genreArray)
+			{
+				let genId = Object.keys(movieGenres).find(key => movieGenres[key] === genre);
+				let genUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${genId}&sort_by=popularity.desc`
+				
+				let response = await fetch(genUrl);
+				let data = await response.json();
+				console.log(data.results[0]);
+				
+				let sliderContainer = document.createElement("div");
+				sliderContainer.className = "slide-container";
+				
+				let div = document.createElement("div");
+				div.id = genre;
+				div.className = "movies-slider";
+				
+				let prev = document.createElement("button");
+				sliderContainer.append(prev);
+				prev.outerHTML = "<button class='scroll-btn prev-btn'> &#8249;</button>";
+				let next = document.createElement("button");
+				sliderContainer.append(next);
+				next.outerHTML = "<button class='scroll-btn next-btn'> &#8250;</button>";
+				
+				
+				sliderContainer.append(div);
+				document.querySelector(".movies-section").append(sliderContainer);
+				
+				cardsOfTheGenres[genre] = [];
+				
+				for(index in data.results)
+				{	
+					insertCard(cardsOfTheGenres[genre],index,data,genre);
+				}
+
+			}
 		}
-
-		nextBtn.addEventListener('click', () => {
-			console.log('Next button clicked!');
-			sliderContainer.scrollBy({
-				left: 460,
-				behavior: 'smooth'
-			});
-		});
+		
+		const prevBtns = document.querySelectorAll('.movies-section .scroll-btn.prev-btn');
+		const nextBtns = document.querySelectorAll('.movies-section .scroll-btn.next-btn');
+		console.log(prevBtns);
+		for(prevbtn of prevBtns)
+		{	
+			console.log(prevbtn.parentElement.querySelector(".movies-slider"));
+			const slideArea = prevbtn.parentElement.querySelector(".movies-slider");
+			prevbtn.onclick = function()
+				{
+					slideArea.scrollBy({
+						left: -460,
+						behavior: 'smooth'
+					});
+				}
+		}
+		
+		for(nextbtn of nextBtns)
+		{	
+			const slideArea = nextbtn.parentElement.querySelector(".movies-slider");
+			nextbtn.onclick = function()
+				{
+					slideArea.scrollBy(
+					{
+						left: 460,
+						behavior: 'smooth'
+					});
+				}
+		}
+		
+	}
 	
+	exec();
+
+
+		
 	
